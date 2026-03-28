553e7407d7afb0f1c069edbcdbe001516dc85acd
Matt Salmon <mrsalmon@gmail.com>
2026-03-28 11:25:04 +0200

    Renamed project "title" column to "name"

--- docs/data-dev-scripts.sql
+++ docs/data-dev-scripts.sql
@@ -1,3 +1,17 @@
+-- drop all objects
+drop table audit.project_logs;
+drop table audit.user_logs;
+drop table public.documents;
+drop table public.notes;
+drop table public.tasks;
+drop table public.tickets;
+drop table public.user_firebase_identities;
+drop table public.user_projects;
+drop table public.projects;
+drop table public.search;
+drop table public.users;
+
+-- clear all data
 delete from audit.project_logs;
 delete from user_projects;
 delete from projects;
--- source/web-api/Apotheca.Migrations/Scripts/0201_public.projects.sql
+++ source/web-api/Apotheca.Migrations/Scripts/0201_public.projects.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS projects (
     id          TEXT        NOT NULL,
-    title       TEXT        NOT NULL,
+    "name"      TEXT        NOT NULL,
     summary     TEXT        NULL,
     created_at  TIMESTAMPTZ NOT NULL DEFAULT now(),
 
